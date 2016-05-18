c7907528a40ad02f5913d6d4370f8f5b8cea1d31
Taylor Robie <robieta@udel.edu>
2016-05-18 10:12:06 -0400

    Add files via upload

--- Code/DemonstrationFile.py
+++ Code/DemonstrationFile.py
@@ -25,11 +25,12 @@ import matplotlib.pyplot as plt
 This section of the code reads the files from a given path and writes the info
 to the Demo.p file for later use
 """
-#Path = 'E:/KMC_ToyProblem/'
+##Path is given as an example
+#Path = 'C:/Users/robieta/Downloads/ZacrosWrapper-master/ZacrosWrapper-master/Data/AtoB/'
 #Cnd = KMCut().InitializeCnd()
 #Cnd = RI().ReadAll(Path,Cnd)
 #Cnd = RO().ReadAll(Path,Cnd)
-#KMCut().pickleCnd(Cnd,'Demo')
+#KMCut().pickleCnd(Cnd,'AtoB')
 
 
 
@@ -48,48 +49,50 @@ with the Demo.p file
 This section of the code perfoms a stiffness reduction on the Demo system for 
 the given reduction modes and saves the result in a .p file
 """
-#TypeName = 'Demo'
-#ModeList = ['linear_1_2','linear_2_4','tanh_1_2','tanh_2_4']
-#
-#WallTimeList = [90]*len(ModeList)
-#for i in range(len(ModeList)):
-#    Mode = ModeList[i]
-#    WallTime = WallTimeList[i]
-#
-#    if not os.path.isfile(ut().SystemInformation()['Path']['Data'] + 
-#        'PickledRunStructures/' + 'Recondition_' + TypeName +'_' + Mode + '.p'):
-#        RS().ReconditionCnd(CndIn = KMCut().unpickleCnd(TypeName),Name=TypeName+'_' + Mode 
-#                            ,RunParam = {'Event':1e3,'WallTime':WallTime,'Mode':Mode})
+TypeName = 'AtoB'
+ModeList = ['linear_1_2','linear_2_4','tanh_1_2','tanh_2_4']
+
+WallTimeList = [90]*len(ModeList)
+for i in range(len(ModeList)):
+    Mode = ModeList[i]
+    WallTime = WallTimeList[i]
+
+    if not os.path.isfile(ut().SystemInformation()['Path']['Data'] + 
+        'PickledRunStructures/' + 'Recondition_' + TypeName +'_' + Mode + '.p'):
+        RS().ReconditionCnd(CndIn = KMCut().unpickleCnd(TypeName),Name=TypeName+'_' + Mode 
+                            ,RunParam = {'Event':1e3,'WallTime':WallTime,'Mode':Mode})
 
 
 
 
 """
 This section builds jobs on Farber based on the prior stiffness reduction and
-submits them (Farber only)
+submits them
 """
-#nRareSample = 500
-#nRuns = 10
-#TypeName = 'Demo'
-#ModeList = ['linear_1_2','linear_2_4','tanh_1_2','tanh_2_4']
-#
-#WallTimeList = [90]*len(ModeList)
-#for i in range(len(ModeList)):
-#    Mode = ModeList[i]
-#    OutList = KMCut().unpickleCnd('Recondition_' + TypeName+'_' + Mode)
-#    SDDict = KMCut().InitializeScaleDown()
-#    SDDict['SF'] = OutList['SF']
-#    SDDict['Mode'] = OutList['Cnd']['StiffnessRecondition']['Mode']
-#    SDDict['SDF'] = RS().CalculateScaleDown(BaseCnd='',Mode=SDDict['Mode'],SFIn=SDDict['SF'])['SDF']
-#    Cnd = OutList['Cnd']
-#    
-#    Cnd = KMCut().SetMaxEventNumber(Cnd,np.float(Mode.split('_')[-1]),nRareSample)
-#    BI().BuildJob(Cnd,SDDict=SDDict,nRuns=nRuns,Name = TypeName+'_' + Mode)
-#    if ut().SystemInformation()['OS'] == 'Linux':
-#        JobPath = ut().SystemInformation()['Path']['Data'] + 'JobBuilds/' + TypeName+'_' + Mode + '/'
-#        p = subprocess.Popen("cd " + JobPath + "; " + "chmod 744 ./SubmitKMC.sh;./SubmitKMC.sh"
-#                        , stdout=subprocess.PIPE,shell=True)
-#    sys.stdout.flush()
+nRareSample = 500 # Estimate, can be off depending on the system
+nRuns = 2
+
+for i in range(len(ModeList)):
+    Mode = ModeList[i]
+    OutList = KMCut().unpickleCnd('Recondition_' + TypeName+'_' + Mode)
+    SDDict = KMCut().InitializeScaleDown()
+    SDDict['SF'] = OutList['SF']
+    SDDict['Mode'] = OutList['Cnd']['StiffnessRecondition']['Mode']
+    SDDict['SDF'] = RS().CalculateScaleDown(BaseCnd='',Mode=SDDict['Mode'],SFIn=SDDict['SF'])['SDF']
+    Cnd = OutList['Cnd']
+    
+    ETsS = np.log10(np.sum(SDDict['SF']/SDDict['SDF'])) # Estimated Timescale separation (for run time estimates)
+    Cnd = KMCut().SetMaxEventNumber(Cnd,ETsS,nRareSample)
+    JobPath = ut().SystemInformation()['Path']['Data'] + 'JobBuilds/' + TypeName+'_' + Mode + '/'
+    if not os.path.isdir(JobPath):
+        BI().BuildJob(Cnd,SDDict=SDDict,nRuns=nRuns,Name = TypeName+'_' + Mode)    
+        if ut().SystemInformation()['OS'] == 'Linux':        
+            p = subprocess.Popen("cd " + JobPath + "; " + "chmod 744 ./SubmitKMC.sh;./SubmitKMC.sh"
+                            , stdout=subprocess.PIPE,shell=True)
+            sys.stdout.flush()
+        else:
+            for j in ut().GetDir(JobPath):
+                RunZacros().Run(JobPath + j + '/')
 
 
 
@@ -98,9 +101,7 @@ submits them (Farber only)
 This line goes through the job build folder, detects which jobs have completed
 but have not been parsed, parses them, and saves the result
 """
-#RO().ReadAllJobs()
-
-
+RO().ReadAllJobs()
 
 
 """
@@ -109,23 +110,18 @@ This line reads the output of a parsed job which can then be processed.
 #CndList = RO().ReadJobOutput('Demo_linear_1_2')
 
 
-
-
-
-
-
 """
 This section takes the various stiffness reduction runs and plots them to show
 the effect of stiffness reduction on observed rate.
 """
-#nSites = 672
-#RxnStoich = [1,3,-2.]
-#PropensityStoich = [-2, 1, 0, 0, 0, 0, 0, 0, 0]
-#LineColors = ['k','b']
+nSites = 50
+RxnStoich = [-1,1]
+PropensityStoich = [-1,1,0,0]
+LineColors = ['k','b']
 #yLim = [0,0.0015]
-#RegFun = ['linear_','tanh_']
-#Modes = ['1_2','2_4']
-#PO().PlotReductionComparison('Demo_',Modes,RegFun,nSites,RxnStoich,PropensityStoich,LineColors,yLim)
+RegFun = ['linear_','tanh_']
+Modes = ['1_2','2_4']
+PO().PlotReductionComparison(TypeName + '_',Modes,RegFun,nSites,RxnStoich,PropensityStoich,LineColors)#,yLim)
                             
                             
                             
