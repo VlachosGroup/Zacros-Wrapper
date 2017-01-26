f77fcdece7008ee5a3237f3d7dd366cb5722ab11
mpnunez <mpnunez@udel.edu>
2017-01-26 14:46:47 -0500

    Use one KMC_Run object to write and read

---
+++ PythonCode/Demos/Multiple_trajectories.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 28 13:48:34 2016
+
+@author: mpnun
+"""
+
+import os
+import sys
+
+sys.path.append('/home/vlachos/mpnunez/Github/Zacros-Wrapper-expt/Zacros-Wrapper/PythonCode')
+import Core as zw
+
+################## User input ##################################
+
+zacros_exe = '/home/vlachos/mpnunez/bin/zacros_ZW.x'
+KMC_source = '/home/vlachos/mpnunez/ZacrosWrapper/KMC_data/AtoB/NonStiff/'
+BatchPath = '/home/vlachos/mpnunez/ZacrosWrapper/KMC_data/AtoB/test'
+#Product = 'B'
+n_runs = 16
+
+################################################################
+
+if __name__ == '__main__':                 # Need this line to make parallelization work
+
+    os.system('cls')  
+
+    # Prepare template
+    x = zw.Replicates()
+    x.runtemplate.Path = KMC_source
+    x.runtemplate.exe_file = zacros_exe
+    x.runtemplate.ReadAllInput()
+    x.ParentFolder = BatchPath
+
+    # Build files and run
+    x.n_runs = n_runs
+    x.BuildJobFiles()
+    x.RunAllJobs_parallel_JobArray()
+    
+    # Read results
+    x.ReadMultipleRuns()
