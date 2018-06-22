ebb679fb0523c714206f572001aa8c45af0ca64e
Yifan Wang <wangyf@udel.edu>
2018-06-22 14:00:12 -0400

    minor change

--- zacros_wrapper/KMC_Run.py
+++ zacros_wrapper/KMC_Run.py
@@ -380,6 +380,12 @@ class kmc_traj():
         g_patch = mat.patches.Patch(color = 'green', label = 'net')
         
         plt.legend(['fwd', 'rev', 'net'], bbox_to_anchor = (1.05, 1),loc= 'upper left', prop={'size':12},frameon=False)
+		 ax = plt.gca()
+		
+		 leg = ax.get_legend()
+		 leg.legendHandles[0].set_color('red')
+		 leg.legendHandles[1].set_color('yellow')
+		 leg.legendHandles[1].set_color('yellow')
         
                
         plt.tight_layout()
