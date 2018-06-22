7ffe770e96f0ff296c7a7119d2a09bcc1c9235b9
Yifan Wang <wangyf@udel.edu>
2018-06-22 13:33:37 -0400

    Minor change

--- zacros_wrapper/KMC_Run.py
+++ zacros_wrapper/KMC_Run.py
@@ -335,7 +335,7 @@ class kmc_traj():
         
         store_ind = 0       # index of where the data is stored
         
-		r_patch = mat.patches.Patch(color = 'red', label = 'fwd')
+		 r_patch = mat.patches.Patch(color = 'red', label = 'fwd')
         b_patch = mat.patches.Patch(color = 'blue', label = 'rev')
         g_patch = mat.patches.Patch(color = 'green', label = 'net')
         plt.legend(handles = [r_patch, b_patch, g_patch ], bbox_to_anchor = (1.05, 1),loc= 'upper left', prop={'size':12},frameon=False)
