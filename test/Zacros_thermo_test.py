d7998b162f491475af45078787c47406838726c0
Yifan Wang <wangyf@udel.edu>
2018-07-31 17:59:43 -0400

    changes on test directory

--- test/Zacros_thermo_test.py
+++ test/Zacros_thermo_test.py
@@ -7,19 +7,25 @@ Created on Sat May 20 15:00:49 2017
 
 import sys
 import os
-#sys.path.append('C:\\Users\\wittregr\\Documents\\Python Scripts')
-sys.path.append('C:\\Users\\wittregr\\Documents\\GitHub\\Zacros-Wrapper\\zacros_wrapper')
+
+HomePath = os.path.expanduser('~')
+sys.path.append(os.path.join(HomePath,'Documents','GitHub'))
+sys.path.append(os.path.join(HomePath,'Documents','GitHub', 'Zacros-Wrapper'))
+
 import zacros_wrapper as zw
+
+
 x = zw.kmc_traj()
-#x.Path = os.path.join('C:\\Users\\wittregr\\Documents\\Python Scripts', 'Input')
-x.Path = os.path.join(os.getcwd(), 'Input')
+
+x.Path = os.path.join(os.getcwd(), 'input')
 x.ReadAllInput()
-print(x.mechin.rxn_list[0].variant_list[0].pe_ratio)
-print(x.mechin.rxn_list[3].variant_list[0].activ_eng)
+
+
+
 T = 650
 filepath = os.path.join(x.Path, "Zacros_Species_Energy.xlsx")
 x.mechin.CalcThermo(filepath,T)
-print(x.mechin.rxn_list[0].variant_list[0].pe_ratio)
-print(x.mechin.rxn_list[3].variant_list[0].activ_eng)
-x.Path = os.path.join(os.getcwd(), 'Output')
+
+
+x.Path = os.path.join(os.getcwd(), 'output')
 x.WriteAllInput()
