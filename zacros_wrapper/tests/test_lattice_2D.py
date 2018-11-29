c686291c2f2cce82ee4ae894e83500019e3f248f
Bharat Medasani <mbkumar@gmail.com>
2018-11-29 16:18:02 -0500

    Test suit initial commit

---
+++ zacros_wrapper/tests/test_lattice_2D.py
@@ -0,0 +1,20 @@
+import os
+import unittest
+
+from zachros_wrapper.lattice_2D import Lattice, Site, PeriodicSite, \
+        ZachrosLattice
+
+
+class SiteTest(unittest.TestCase):
+    pass
+
+
+class PeriodicSiteTest(unittest.TestCase):
+    pass
+
+
+class LatticeTest(unittest.TestCase):
+    pass
+
+class ZachrosLatticeTest(unittest.TestCase):
+    pass
